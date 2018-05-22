ec4a186e2cd8115301cce25b6fa5912a03118cb3
JeffChoi <ihello0720@gmail.com>
2018-05-22 16:20:53 +0900

    공지 작성 페이지 만들기..

--- src/Helper/Server.js
+++ src/Helper/Server.js
@@ -8,7 +8,7 @@ const acceptSleepOutUrl = '/out/sleep/accept'; // 외박 승인
 const unAcceptGoOutUrl = '/out/go/list/unaccept'; // 승인되지 않은 외출 신청 조회
 const unAcceptSleepOutUrl = '/out/sleep/list/unaccept'; // 승인되지 않은 외박 신청 조회
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ0ZWFjaGVyXzFAZGdzdy5ocy5rciIsImNsYXNzSWR4IjpudWxsLCJhdXRoIjowLCJpYXQiOjE1MjYxMzUzODEsImV4cCI6MTUyNjc0MDE4MSwiaXNzIjoiamVmZmNob2kuY29tIiwic3ViIjoidG9rZW4ifQ.PbMnRJF5uolK3YVoP_Xqh4WKagwZLa6ZpNsFHsyguxc';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ0ZWFjaGVyXzFAZGdzdy5ocy5rciIsImNsYXNzSWR4IjpudWxsLCJhdXRoIjowLCJpYXQiOjE1MjY4NjU2NTUsImV4cCI6MTUyNzQ3MDQ1NSwiaXNzIjoiamVmZmNob2kuY29tIiwic3ViIjoidG9rZW4ifQ.tIRF_zeOzP5famiSIg8Np78AF74LKhIoYWpnCpwNrV8';
 
 
 const GetGoOutData = async () => { // 외출 데이터를 가지고 온다.
