ce592ffa003caacc6c39d897096026d4fc4138f0
JeffChoi <ihello0720@gmail.com>
2018-05-22 16:25:32 +0900

    신청 페이지 컴포넌트로 분리

---
+++ src/Components/Request/RequestPresenter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Header from 'Components/Header';
+import GoOut from 'Components/GoOut';
+import SleepOut from 'Components/SleepOut';
+
+const Container = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    margin-bottom: 50px;
+`;
+
+const GoOutContainer = styled.div`
+    grid-start-column: 1;
+`;
+
+const SleepOutContainer = styled.div`
+    grid-start-column: 2;
+`;
+
+const Title = styled.h3`
+    padding-left: 40px;
+    font-weight: bold;
+    font-size: 24px;
+    margin-bottom: 50px;
+`;
+
+const RequestPresenter = () => {
+    return (
+        <Fragment>
+            <Header />
+            <Container>
+                <GoOutContainer>
+                    <Title>외출 신청 학생 리스트</Title>
+                    <GoOut />
+                </GoOutContainer>
+                <SleepOutContainer>
+                    <Title>외박 신청 학생 리스트</Title>
+                    <SleepOut />
+                </SleepOutContainer>
+            </Container>
+        </Fragment>
+    );
+}
+
+export default RequestPresenter;
